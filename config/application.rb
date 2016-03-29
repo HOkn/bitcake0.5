65bc8f7128d59e2b8c4ea5f01da0acf005c3b3bd
HOkn <haruokny@gmail.com>
2016-03-30 04:14:01 +0900

    [add] css to sass setting

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Bitcake05
   class Application < Rails::Application
 
 
-    config.sass.preferred_syntax = sass
+    config.sass.preferred_syntax = :sass
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
