90ece2090575f393d3032a1a44811d48df354f58
HOkn <haruokny@gmail.com>
2016-03-30 04:44:14 +0900

    [add] validation users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..24 }
+   validates :email, presence: true, uniqueness: { case_sensitive: false }
+   validates :password, confirmation: true, length: { in: 6..24 }, if: :password
+   validates :password_confirmation, presence: true, if: :password
+
 end
